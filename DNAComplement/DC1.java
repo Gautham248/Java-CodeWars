ad508d104c3234bb0e70930cdd8e34d5fe6aa833
Gautham <95111246+Gautham248@users.noreply.github.com>
2022-06-20 21:30:45 +0530

    Create DC1.java

---
+++ DNAComplement/DC1.java
@@ -0,0 +1,10 @@
+public class DnaStrand {
+  public static String makeComplement(String dna) {
+    return dna.replace("A","B")
+              .replace("C","D")
+              .replace("T","A")
+              .replace("G","C")
+              .replace("B","T")
+              .replace("D","G");
+  }
+}
