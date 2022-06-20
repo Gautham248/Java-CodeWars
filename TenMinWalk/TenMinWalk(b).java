3e35889bb3f2cb69f42500342dc064984852be87
Gautham <95111246+Gautham248@users.noreply.github.com>
2022-06-20 20:54:21 +0530

    Create TenMinWalk(b).java

---
+++ TenMinWalk/TenMinWalk(b).java
@@ -0,0 +1,7 @@
+public class TenMinWalk {
+    public static boolean isValid(char[] walk) {
+        String s = new String(walk);
+        return s.chars().filter(p->p=='n').count()==s.chars().filter(p->p=='s').count()&&
+                s.chars().filter(p->p=='e').count()==s.chars().filter(p->p=='w').count()&&s.chars().count()==10;
+    }
+}
